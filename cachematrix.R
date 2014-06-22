5795903b48cf8a56d1392f983399defca73ac20e
cjcourier <cjcourier@gmail.com>
2014-06-22 12:18:09 -0700

    Completion of programming assignment number 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#' Helper function that stores a matrix and can caches the inverse
+#'
+#' Retruns a list that contains the matrix and the following helper inner 
+#' functions
+#'  - set(matrix)			 : Set the current matrix
+#'  - matrix get()			 : Get the current matrix
+#'  - setinvmatrix(matrix)	 : Set the inverse matrix (i.e. solve(matrix))
+#'  - matrix getinvmatrix()  : Get the inverse matrix
+#'
+#' @param x	matrix
+#' @examples
+#' makeCacheMatrix()
+#' makeCacheMatrix(matrix(4:1,2,2))
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        im <- NULL
+        set <- function(y) {
+                x <<- y
+                im <<- NULL
+        }
+        get <- function() x
+        setinvmatrix <- function(invMatrix) im <<- invMatrix
+        getinvmatrix <- function() im
+        list(set = set, get = get,
+             setinvmatrix = setinvmatrix,
+             getinvmatrix = getinvmatrix)
 }
 
-
-## Write a short comment describing this function
+#' Calculates the inverse matrix for a matrix of type makeCacheMatrix and
+#' stores the matrix inverse to the makeCacheMatrix with the 
+#' makeCacheMatrix.setinvmatrix() function.
+#'
+#' Retruns the inverse matrix 
+#'
+#' @param x	makeCacheMatrix matrix
+#' @examples
+#' cacheSolve(my.cacheMatrix)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        imatrix <- x$getinvmatrix()
+        if(!is.null(imatrix)) {
+                message("getting cached data")
+                return(imatrix)
+        }
+        data <- x$get()
+        imatrix <- solve(data, ...)
+        x$setinvmatrix(imatrix)
+        imatrix
 }
